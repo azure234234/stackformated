0cb9ccbd91892316b8e49f4a00f871e104bdf6fb
root <pawan.pandey79797979@gmail.com>
2018-03-09 13:10:52 +0000

    formatted

---
+++ search/roles/searchinstall/files/searchinstall.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+##ElasticSearch##
+ip1=$1  
+ip2=$2 
+ip3=$3 
+id1=$4
+id2=$5
+id3=$6
+
+sudo yum -y update
+sudo yum -y install firewalld
+sudo yum -y install wget
+sudo yum -y install java-1.8.0-openjdk.x86_64      #Installing Java 8
+
+sudo wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.2.1.rpm
+
+sudo rpm --install elasticsearch-5.2.1.rpm
+
+if [ $(hostname -I) = $ip1 ]; then
+###
+sudo sed -i 's/#cluster.name:.*/cluster.name: eviCoreServices/' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#node.name:.*/node.name: node-1/' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#network.host:.*/network.host: "'$id1'"/' /etc/elasticsearch/elasticsearch.yml 
+sudo sed -i 's/#discovery.zen.ping.unicast.hosts:.*/discovery.zen.ping.unicast.hosts: ["'$id1'", "'$id2'", "'$id3'"] /' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#discovery.zen.minimum_master_nodes:.*/discovery.zen.minimum_master_nodes: 2/' /etc/elasticsearch/elasticsearch.yml
+###
+sudo systemctl daemon-reload
+sudo systemctl enable elasticsearch.service
+
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-port=9200/tcp
+sudo firewall-cmd --permanent --add-port=9300/tcp
+sudo firewall-cmd --reload
+
+sudo systemctl start elasticsearch.service
+sudo systemctl stop elasticsearch.service
+
+
+
+##Install Kibana##
+sudo wget https://artifacts.elastic.co/downloads/kibana/kibana-5.2.1-x86_64.rpm
+
+sudo rpm --install kibana-5.2.1-x86_64.rpm
+
+sudo systemctl daemon-reload
+sudo systemctl enable kibana.service
+
+##
+sudo sed -i 's/#server.host:.*/server.host: "'$id1'"/' /etc/kibana/kibana.yml
+sudo sed -i 's/#elasticsearch.url:.*/elasticsearch.url: "http://'$id1':9200"/' /etc/kibana/kibana.yml
+##
+
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-port=5601/tcp
+sudo firewall-cmd --reload	
+
+sudo systemctl start kibana.service
+sudo systemctl stop kibana.service
+
+fi 
+
+if [ $(hostname -I) = $ip2 ]; then
+
+###
+sudo sed -i 's/#cluster.name:.*/cluster.name: eviCoreServices/' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#node.name:.*/node.name: node-2/' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#network.host:.*/network.host: "'$id2'"/' /etc/elasticsearch/elasticsearch.yml 
+sudo sed -i 's/#discovery.zen.ping.unicast.hosts:.*/discovery.zen.ping.unicast.hosts: ["'$id1'", "'$id2'", "'$id3'"] /' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#discovery.zen.minimum_master_nodes:.*/discovery.zen.minimum_master_nodes: 2/' /etc/elasticsearch/elasticsearch.yml
+###
+
+sudo systemctl daemon-reload
+sudo systemctl enable elasticsearch.service
+
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-port=9200/tcp
+sudo firewall-cmd --permanent --add-port=9300/tcp
+sudo firewall-cmd --reload
+
+sudo systemctl start elasticsearch.service
+sudo systemctl stop elasticsearch.service
+
+fi
+
+if [ $(hostname -I) = $ip3 ]; then
+
+###
+sudo sed -i 's/#cluster.name:.*/cluster.name: eviCoreServices/' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#node.name:.*/node.name: node-3/' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#network.host:.*/network.host: "'$id3'"/' /etc/elasticsearch/elasticsearch.yml 
+sudo sed -i 's/#discovery.zen.ping.unicast.hosts:.*/discovery.zen.ping.unicast.hosts: ["'$id1'", "'$id2'", "'$id3'"] /' /etc/elasticsearch/elasticsearch.yml
+sudo sed -i 's/#discovery.zen.minimum_master_nodes:.*/discovery.zen.minimum_master_nodes: 2/' /etc/elasticsearch/elasticsearch.yml
+###
+
+sudo systemctl daemon-reload
+sudo systemctl enable elasticsearch.service
+
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-port=9200/tcp
+sudo firewall-cmd --permanent --add-port=9300/tcp
+sudo firewall-cmd --reload
+
+sudo systemctl start elasticsearch.service
+sudo systemctl stop elasticsearch.service
+
+fi
+
